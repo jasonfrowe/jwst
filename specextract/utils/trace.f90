a4c85a720ef1c4c1f6f17804831bea54553e3dcb
jasonfrowe <jasonfrowe@gmail.com>
2016-07-27 17:00:36 -0400

    removed dependence on external PSF FITS file

--- specextract/utils/trace.f90
+++ specextract/utils/trace.f90
@@ -3,7 +3,8 @@ subroutine trace(naxes,Image,bpix,nline,nTrace,dTrace,bf,solpsf,posguess)
 !generates trace - Version 2.0
 use precision
 implicit none
-integer :: nkeys,nkeysmax,nKsize,i,nline,nksd2,xm,xp,j,nmaxf,k,nTrace,  &
+integer, parameter :: nKsize=64
+integer :: nkeys,nkeysmax,i,nline,nksd2,xm,xp,j,nmaxf,k,nTrace,  &
    nTp,jj,ncut,ncutpsf,ncutd2,ilinkpsf,nbin
 integer, dimension(2) :: naxes,knaxes
 integer, allocatable, dimension(:) :: isol,isolfirst
@@ -11,7 +12,8 @@ real(double) :: Kmin,Kmax,bpix,maxf,S,Sxy,Sxx,Sx,Sy,df,bcut,f,          &
    triplegaussian,sq2pi,pi,tracetest,tcut,sky,std,snr,signal
 real(double), dimension(:) :: posguess
 real(double), dimension(:,:) :: Image,dTrace,bf,solpsf
-real(double), allocatable, dimension(:) :: line,lpsf,lpsftemp,psfwork,  &
+real(double), dimension(nKsize) :: lpsf
+real(double), allocatable, dimension(:) :: line,lpsftemp,psfwork,  &
    sol,model,solnew,amp,solfirst,solbin
 real(double), allocatable, dimension(:,:) :: Kernel,psf
 character(80) :: kfile
@@ -19,20 +21,40 @@ character(80), allocatable, dimension(:) :: header
 !plotting
 real :: pmin
 real, allocatable, dimension(:) :: px,py
+data  lpsf /3.746300735497921E-006,7.640107659745432E-006,4.542999963486538E-006, &
+  4.929265102315838E-006,  9.714837387708730E-006,  5.671904909021475E-006, &
+  4.548023730510664E-006,  1.022713226439542E-005,  6.886893882507295E-006, &
+  8.177790144225927E-006,  1.357109057534278E-005,  8.916710340478584E-006, &
+  1.239566539967818E-005,  2.781745489985332E-005,  2.509716449416999E-005, &
+  2.631011432652208E-005,  4.830151269574756E-005,  5.380108778450451E-005, &
+  7.547263667725956E-005,  1.022118883162726E-004,  1.420077972523748E-004, &
+  2.362241206079752E-004,  3.385566380821325E-004,  5.477043893594158E-004, &
+  6.696818098559376E-004,  5.493319867611035E-004,  4.720754680409556E-004, &
+  2.991750642213908E-004,  3.058475983204190E-004,  3.109660592775787E-004, &
+  2.226914950899106E-004,  2.979418360802288E-004,  3.397708704659941E-004, &
+  2.990017218531538E-004,  2.758223087866440E-004,  3.294162992516503E-004, &
+  2.381257536346881E-004,  3.407167609725814E-004,  5.361983993812380E-004, &
+  6.230353641937803E-004,  6.140843798414508E-004,  5.070604643273580E-004, &
+  3.306009460586345E-004,  2.371751859966409E-004,  1.155928608405077E-004, &
+  6.370671124544813E-005,  7.242587988226523E-005,  3.417951946560471E-005, &
+  2.799611461752616E-005,  3.403609616187131E-005,  1.659919620922157E-005, &
+  1.873137450902895E-005,  1.825263581423098E-005,  9.144579730557822E-006, &
+  8.962197003747896E-006,  9.059127708432868E-006,  5.408733372069818E-006, &
+  7.623564329893584E-006,  7.990429056435600E-006,  4.048852234816991E-006, &
+  6.761537376720472E-006,  7.205561307621622E-006,  4.591548022681025E-006, &
+  4.112999194311184E-006/
 
 interface
-   subroutine getfits(Refname,naxes,Ref,Rmin,Rmax,nkeys,header,bpix)
-      use precision
-      implicit none
-      integer :: nkeys
-      integer, dimension(2), intent(inout) :: naxes
-      real(double), intent(inout) :: Rmin,Rmax,bpix
-      real(double), dimension(:,:), intent(inout) :: Ref
-      character(80), intent(inout) :: Refname
-      character(80), dimension(:), intent(inout) :: header
-   end subroutine getfits
-end interface
-interface
+!   subroutine getfits(Refname,naxes,Ref,Rmin,Rmax,nkeys,header,bpix)
+!      use precision
+!      implicit none
+!      integer :: nkeys
+!      integer, dimension(2), intent(inout) :: naxes
+!      real(double), intent(inout) :: Rmin,Rmax,bpix
+!      real(double), dimension(:,:), intent(inout) :: Ref
+!      character(80), intent(inout) :: Refname
+!      character(80), dimension(:), intent(inout) :: header
+!   end subroutine getfits
    subroutine loadPSFinit(ntrace,sol,ncutpsf,nline,dtrace,line)
       use precision
       implicit none
@@ -40,8 +62,6 @@ interface
       real(double), dimension(:), intent(inout) :: sol,line
       real(double), dimension(:,:), intent(inout) :: dtrace
    end subroutine loadPSFinit
-end interface
-interface
    subroutine psfmodel1d(npt,model,ntrace,sol)
       use precision
       implicit none
@@ -49,8 +69,6 @@ interface
       real(double), dimension(:), intent(in) :: sol
       real(double), dimension(:), intent(inout) :: model
    end subroutine psfmodel1d
-end interface
-interface
    subroutine modelline(npt,line,ntrace,sol,isol,ilinkpsf,nline)
       use precision
       implicit none
@@ -58,8 +76,6 @@ interface
       integer, dimension(:), intent(inout) :: isol
       real(double), dimension(:), intent(inout) :: line,sol
    end subroutine modelline
-end interface
-interface
    function getsky(naxes,Image,std)
       use precision
       implicit none
@@ -70,7 +86,7 @@ interface
 end interface
 
 !constants
-nKsize=64        !size of Kernel for import (note.. this gets resized)
+!nKsize=64        !size of Kernel for import (note.. this gets resized)
 nksd2=nKsize/2 !precompute size of PSF/2
 Pi=acos(-1.d0)       !define Pi
 sq2pi=sqrt(2.0d0*pi) !define sqrt(2*pi)
@@ -87,18 +103,18 @@ if(ncutpsf.gt.nKsize)then  !check ncutpsf value is valid
 endif
 ncutd2=ncutpsf/2 !precompute ncutd2
 
-!read in a Kernel to be used as a guess for the PSF
-kfile="Kernels1/psf_2100nm_x10_oversampled.fits"
-nkeysmax=800 !maximum number of lines in the header we will accept.
-allocate(header(nkeysmax)) !allocate space for header (we really don't need it)
-allocate(Kernel(nKsize,nKsize))  !allocate space for Kernel
-call getfits(kfile,knaxes,Kernel,Kmin,Kmax,nkeys,header,bpix) !read in FITS
-write(0,*) "knaxes: ",knaxes
-
-!generate 1-D PSF for cross-correlation
-allocate(lpsf(nKsize),psfwork(nKsize))
-lpsf=Sum(Kernel,2)
-lpsf=lpsf(nKsize:1:-1) !looks like the PSF needs to be flipped to match spgen
+!!read in a Kernel to be used as a guess for the PSF
+!kfile="Kernels1/psf_2100nm_x10_oversampled.fits"
+!nkeysmax=800 !maximum number of lines in the header we will accept.
+!allocate(header(nkeysmax)) !allocate space for header (we really don't need it)
+!allocate(Kernel(nKsize,nKsize))  !allocate space for Kernel
+!call getfits(kfile,knaxes,Kernel,Kmin,Kmax,nkeys,header,bpix) !read in FITS
+!write(0,*) "knaxes: ",knaxes
+!
+!!generate 1-D PSF for cross-correlation
+!allocate(lpsf(nKsize),psfwork(nKsize))
+!lpsf=Sum(Kernel,2)
+!lpsf=lpsf(nKsize:1:-1) !looks like the PSF needs to be flipped to match spgen
 
 allocate(psf(nKsize,ntrace))
 do i=1,nTrace    !make a trace for each order.
