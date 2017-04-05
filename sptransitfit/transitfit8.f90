d736910d244c5960e3ad652bb1bb601d1e349fcd
jasonfrowe <jasonfrowe@gmail.com>
2017-04-05 13:47:38 -0400

    reading in transit model parameters

--- sptransitfit/transitfit8.f90
+++ sptransitfit/transitfit8.f90
@@ -2,10 +2,22 @@ program transitfit8
 !(c) Jason Rowe 2017 jason.rowe@ubishops.ca
 use precision
 implicit none
-integer iargc,nobsmax,nwvmax,nunit,nobs,nwv,npars,nparsmax
+integer iargc,nobsmax,nwvmax,nunit,nobs,nwv,npars,nparsmax,i,j,         &
+ nplanetmax,nplanet
 real(double), allocatable, dimension(:) :: time,sol
 real(double), allocatable, dimension(:,:) :: flux,solerr
 character(80) :: obsfile,parsfile
+
+interface
+   subroutine getfitpars(nunit,nparsmax,nplanetmax,npars,nplanet,sol,   &
+    solerr)
+      use precision
+      implicit none
+      integer :: nparsmax,npars,nunit,nplanet,nplanetmax
+      real(double), dimension(:) :: sol
+      real(double), dimension(:,:) :: solerr
+   end subroutine getfitpars
+end interface
 
 if(iargc().lt.1)then
    write(6,*) "Usage: transitfit5 <specfile> <modelpars>"
@@ -44,14 +56,21 @@ close(nunit)
 call openfile(nunit,parsfile)
 
 !calculate the number of parameters we are fitting
-call getnumfitpars(nunit,nparsmax)
-write(0,*) "Maximum number of model parameters"
+call getnumfitpars(nunit,nparsmax,nplanetmax)
+!write(0,*) "Maximum number of model parameters: ",nparsmax
+!write(0,*) "Maximum number of planets in model: ",nplanetmax
 
 !allocate variables for model parameters
 allocate(sol(nparsmax),solerr(nparsmax,4))
 
 !read in model parameters
-call getfitpars()
+call getfitpars(nunit,nparsmax,nplanetmax,npars,nplanet,sol,solerr)
+write(0,*) "Number of model parameters read: ",npars
+write(0,*) "Number of planets in model: ",nplanet
+!do i=1,npars
+!   write(6,500) sol(i),(solerr(i,j),j=1,4)
+!   500 format(40(1X,1PE17.10))
+!enddo
 
 !close parameter file
 close(nunit)
--- sptransitfit/utils/getfitpars.f90
+++ sptransitfit/utils/getfitpars.f90
@@ -1,21 +1,30 @@
 !CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
-subroutine getnumfitpars(nunit,npars)
+subroutine getnumfitpars(nunit,npars,nplanetmax)
 !CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
 use precision
 implicit none
 !input vars
-integer :: nunit,npars
+integer :: nunit,npars,nplanetmax
 !local vars
-integer :: i,nvar,filestatus,iltype
+integer :: i,nvar,filestatus,iltype,np
 real(double) :: dumr
 character(3) :: pname
 
+nplanetmax=0 !counting number of planets in the model
 npars=0 !counting number of model parameters
 i=0 !counter for number of observations.
 iltype=0 !if =0, then line describes variable, otherwise it's parameter values
 do
    if (iltype.eq.0) then
       read(nunit,*,iostat=filestatus) pname,nvar
+      if((pname(1:2).eq.'EP').or.(pname(1:2).eq.'PE').or.               &
+       (pname(1:2).eq.'BB').or.(pname(1:2).eq.'RD').or.                 &
+       (pname(1:2).eq.'EC').or.(pname(1:2).eq.'ES').or.                 &
+       (pname(1:2).eq.'KR').or.(pname(1:2).eq.'TE').or.                 &
+       (pname(1:2).eq.'EL').or.(pname(1:2).eq.'AL'))then
+         read(pname(3:3),*) np !read in planet number
+         nplanetmax=max(nplanetmax,np) !get numbe of planets in model
+      endif
    else
       read(nunit,*,iostat=filestatus) dumr
    endif
@@ -43,12 +52,67 @@ return
 end subroutine getnumfitpars
 
 !CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
-subroutine getfitpars()
+subroutine getfitpars(nunit,nparsmax,nplanetmax,npars,nplanet,sol,      &
+ solerr)
 !CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
 use precision
 implicit none
+!import vars
+integer :: nunit,nparsmax,npars,nplanet,nplanetmax
+real(double), dimension(:) :: sol
+real(double), dimension(:,:) :: solerr
+!local vars
+integer :: i,j,k,iltype,nvar,filestatus,np
+real(double), allocatable, dimension(:) :: parsin
+character(3) :: pname
 
-write(0,*) "hello"
+allocate(parsin(5))
+
+nplanet=0 !counting number of planets in model
+npars=0 !counting number of model parameters
+i=0 !counter for number of observations.
+j=0 !counter for number of model parameters
+iltype=0 !if =0, then line describes variable, otherwise it's parameter values
+do
+   if (iltype.eq.0) then
+      read(nunit,*,iostat=filestatus) pname,nvar
+      if((pname(1:2).eq.'EP').or.(pname(1:2).eq.'PE').or.               &
+       (pname(1:2).eq.'BB').or.(pname(1:2).eq.'RD').or.                 &
+       (pname(1:2).eq.'EC').or.(pname(1:2).eq.'ES').or.                 &
+       (pname(1:2).eq.'KR').or.(pname(1:2).eq.'TE').or.                 &
+       (pname(1:2).eq.'EL').or.(pname(1:2).eq.'AL'))then
+         read(pname(3:3),*) np !read in planet number
+         nplanet=max(nplanet,np) !get numbe of planets in model
+      endif
+   else
+      read(nunit,*,iostat=filestatus) (parsin(k),k=1,5)
+   endif
+   if(filestatus == 0) then
+      i=i+1
+      if(iltype.eq.0)then
+         npars=npars+nvar
+         iltype=nvar
+      else
+         j=j+1
+         if(j.gt.nparsmax)then !make sure we are not overunning array
+            write(0,*) "nparsmax is too small. This should not happen!"
+            write(0,*) "nparsmax: ",nparsmax,j
+            stop
+         else
+            sol(j)=parsin(1)
+            solerr(j,:)=parsin(2:5)
+            iltype=iltype-1
+         endif
+      endif
+   elseif(filestatus == -1) then
+      exit  !successively break from data read loop.
+   else
+      write(0,*) "File Error!! Line:",i+1
+      write(0,900) "iostat: ",filestatus
+      900 format(A8,I3)
+      stop
+   endif
+enddo
 
 return
 end subroutine getfitpars
