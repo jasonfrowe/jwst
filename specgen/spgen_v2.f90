92701d0eb14c14bab0a8426973a1846e55de7283
Jason Rowe <jasonfrowe@gmail.com>
2020-02-03 17:34:23 -0500

    commandline + rand

--- specgen/spgen_v2.f90
+++ specgen/spgen_v2.f90
@@ -9,6 +9,9 @@ character(200), dimension(3) :: fileout !name of FITS files
 !file name vars
 integer :: pid,onum,vnum,gnum,spseq,anumb,enum
 character(8) :: detectorname,prodtype
+!random number vars
+integer, dimension(3) :: now
+integer :: seed
 !local vars
 integer :: i,iargc
 real(double) :: xout, yout,rv
@@ -22,16 +25,60 @@ interface
 	end subroutine writefitsphdu
 end interface
 
+!CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
+!Command line arguments
+!CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
 if(iargc().lt.3)then
-   write(0,*) "Usage: spgen <specmodel> <noversample> <planetmodel> [b] [time]"
+   write(0,*) "Usage: spgen <specmodel> <noversample> <planetmodel>"
    write(0,*) "   <specmodel> - Atlas-9 stellar model"
    write(0,*) " <noversample> - is new sampling for Kernel (must be > 0)"
    write(0,*) " <planetmodel> - name of planet model (A, rprs)"
    write(0,*) "           [b] - impact parameter - optional (must be > 0)"
-   write(0,*) "        [time] - observation timestamp to embed in header"
-   write(0,*) " [output.fits] - filename for output"
    stop
 endif
+
+if(iargc().ge.4)then
+   call getarg(4,cline)
+   read(cline,*) b
+   if(b.lt.0.0d0)then
+      write(0,*) "b must be positive"
+      stop
+   endif
+else
+   !default impact parameter
+   b=2.0!0.3589
+endif
+
+noversample=1 !now a commandline-parameter
+!get oversampling from commandline
+call getarg(2,cline)
+read(cline,*) noversample !read in noversample
+if(noversample.le.0)then
+   write(0,*) "noversample must be greater than zero"
+   stop
+endif
+
+!read in a model spectrum
+call getarg(1,modelfile)
+nunit=11 !unit number for data spectrum
+open(unit=nunit,file=modelfile,iostat=filestatus,status='old')
+if(filestatus>0)then !trap missing file errors
+   write(0,*) "Cannot open ",modelfile
+   stop
+endif
+
+!read in a model spectrum
+call getarg(1,modelfile)
+nunit=11 !unit number for data spectrum
+open(unit=nunit,file=modelfile,iostat=filestatus,status='old')
+if(filestatus>0)then !trap missing file errors
+   write(0,*) "Cannot open ",modelfile
+   stop
+endif
+
+!CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
+!  Model Parameters 
+!CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
 
 !image dimensions
 xout=2048  !dimensions for output image.
@@ -40,7 +87,12 @@ yout=256
 !parameters that control the simulation
 rv=0.0 !radial velocity shift (m/s)
 
+!parameter controling modeltype
+nmodeltype=2 !1=BT-Settl, 2=Atlas-9+NL limbdarkening
+
+!CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
 !file naming
+!CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
 pid = 1 !programID
 onum = 1 !observation number
 vnum = 1 !visit number
@@ -52,7 +104,9 @@ detectorname = 'NISRAPID' !convert this
 prodtype='cal'
 call getfilename(pid,onum,vnum,gnum,spseq,anumb,enum,detectorname,prodtype,fileout)
 
+!CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
 !create FITS files and insert primary HDU for each data type. 
+!CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
 ! 1 - simulation with no convolution,  Native resolution
 ! 2 - simulation with convolution, native resolution
 ! 3 - simulation with convolution, over-sampled resolution
@@ -60,6 +114,12 @@ call writefitsphdu(fileout(1),funit(1))
 call writefitsphdu(fileout(2),funit(2))
 call writefitsphdu(fileout(3),funit(3))
 
+!CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
+! Random Number Initialization
+!CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC!Initialization of random number
+call itime(now)
+seed=abs(now(3)+now(1)*now(2)+now(1)*now(3)+now(2)*now(3)*100)
+dumr=ran2(-seed)
 
 
 
