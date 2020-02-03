aaff0ff02d9387707743a0351c979001009c298e
Jason Rowe <Jason.Rowe@ubishops.ca>
2020-02-03 17:52:34 -0500

    fixing closefits

--- specgen/utils/closefits.f90
+++ specgen/utils/closefits.f90
@@ -4,9 +4,13 @@ use precision
 implicit none
 integer :: funit,status
 
+status=0
+
 !close fits file
 call ftclos(funit,status)
+write(0,*) funit,status
 call ftfiou(funit,status)
+write(0,*) funit,status
 
 return
 end subroutine
