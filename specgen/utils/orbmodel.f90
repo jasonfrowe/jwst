369e3a96c0a1ac7f49b2c35af32958856b6fe224
Jason Rowe <jasonfrowe@gmail.com>
2020-02-22 09:28:28 -0500

    fixed typo

--- specgen/utils/orbmodel.f90
+++ specgen/utils/orbmodel.f90
@@ -8,7 +8,7 @@ real(double) :: t,sol(6),orbmodel
 real(double) :: Pi,tPi,pid2,G
 real(double) :: per,bs2,b,ecw,esw,eccn,w,adrs,incl,epoch,Eanom,phi0
 real(double) :: phi,Manom,Tanom,drs,x2,y2,b
-real(double) :: trueanomaly,distanc
+real(double) :: trueanomaly,distance
 
 Pi=acos(-1.d0)!define Pi and 2*Pi
 tPi=2.0d0*Pi 
