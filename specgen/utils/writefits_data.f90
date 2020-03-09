b85dbe987628e101edf86b8f9287e5a552821e5d
Jason Rowe <Jason.Rowe@ubishops.ca>
2020-03-09 12:02:50 -0400

    Updating to BITPIX=-16 to save disk space

--- specgen/utils/writefits_data.f90
+++ specgen/utils/writefits_data.f90
@@ -21,7 +21,8 @@ if (firstpix.eq.1) then !if firstpix==1, then this is the first call. Initiate e
 
    !BITPIX = 16 means that the image pixels will consist of 16-bit
    !integers.  The size of the image is given by the NAXES values.
-   bitpix=-32 !using float-doubles. 
+   !bitpix=-32 !using float-doubles. 
+   bitpix=-16 !using float-single.
    naxis=4 !JWST obs have 4 axes
    allocate(naxes(naxis)) 
    naxes(1) = xout
@@ -44,7 +45,8 @@ npixels =xout*yout
 if(firstpix+npixels.le.maxpixels)then !check for potential overflows
    do k=1,ngroup
       dngrpfac=dble(k)/dble(ngroup)
-      call ftpprd(funit,group,firstpix,npixels,pixels(1:xout,yout:1:-1)*dngrpfac,status)
+      !call ftpprd(funit,group,firstpix,npixels,pixels(1:xout,yout:1:-1)*dngrpfac,status)
+      call ftppre(funit,group,firstpix,npixels,pixels(1:xout,yout:1:-1)*dngrpfac,status)
       firstpix=firstpix+npixels
    enddo
 else
